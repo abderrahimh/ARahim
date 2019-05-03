936bd256fac42862d7142020665344f72250a4f6
ARahim <abderrahimhechachena@yahoo.co.uk>
2019-05-03 02:53:27 +0000

    code cleaning

--- LTW.java
+++ LTW.java
@@ -2,19 +2,22 @@ import java.util.ArrayList;
 import java.util.stream.IntStream;
 
 public class BWT {
-    //1) ====================== get unique characters in a string ==============
+    // the input string is the string containing the original text to be B W Transformed
+    // some functions use arrays instead of strings for access ease.
+    // all first five function containing the word unique are part of pre-calculations to reduce time.
+    //1) =========== return the unique characters from the input string  as an char[] array ==============
     public static char[] getUniqueCharacters(String string, int stringLength){
-        String temp = "";
-        char current;
+        String uniqueCharacters = "";
+        char currentChar;
         for (int i = 0; i < stringLength; i++){
-            current = string.charAt(i);
-            if (temp.indexOf(current) >= 0){}else{
-                temp += current; // adding characters
+            currentChar = string.charAt(i);
+            if (uniqueCharacters.indexOf(currentChar) >= 0){}else{
+                uniqueCharacters += currentChar; // adding characters
             }
         }
-        return temp.toCharArray();
+        return uniqueCharacters.toCharArray();
     }
-    //2) ====================== get all zero positions in a string ==================
+    //2) ========= get all zero positions in the original string starting from character.. ==================
     public  static int[] getUniqueVectorZeroPositions(String string, int stringLength, Character character){
         ArrayList<Integer> list = new ArrayList<>();
         for(int i = 0; i < stringLength; i++){
@@ -24,7 +27,7 @@ public class BWT {
         }
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
-    //3) ============================= do the above for every unique vector ===========================================
+    //3) ============================= do the above for every unique string ======================================
     public static int[][] getUniqueVectorsZeroPositions(String string, char[] uniqueCharactersSet,int stringLength){
         int resultList[][] = new int[256][];
         for(char character : uniqueCharactersSet) {
@@ -33,25 +36,25 @@ public class BWT {
         return resultList;
     }
 
-    //4) ============================= get non zero weights for every unique vector ====================================
+    //4) ============================= get non zero weights for every unique string ================================
     public static int[][] getUniqueVectorsWeights(char[] arr, int stringLength, char[] uniqueChars){
-        int[] result = new int[stringLength];
-        int resultList[][] = new int[256][];
-        char col;
-        for(char character : uniqueChars) {
+        int[] columnWeights = new int[stringLength];
+        int[][] uniqueColumnsWeights = new int[256][];
+        char columnName;
+        for(char rowName : uniqueChars) {
             for (int i = 0; i < stringLength; i++) {
-                col = arr[i];
-                if (character > col) {
-                    result[i] = 1;
+                columnName = arr[i];
+                if (rowName > columnName) {
+                    columnWeights[i] = 1;
                 }else{
-                if (character < col) {
-                    result[i] = -1;
+                if (rowName < columnName) {
+                    columnWeights[i] = -1;
                 }}
             }
-            resultList[character] = result;
-            result = new int[stringLength]; // start next cycle with an empty object.
+            uniqueColumnsWeights[rowName] = columnWeights;
+            columnWeights = new int[stringLength]; // start next cycle with an empty object.
         }
-        return resultList;
+        return uniqueColumnsWeights;
     }
     //5) =============================== calculate unique vectors sums ================================================
     public  static int[] getUniqueVectorsSum(int[][] vectorsOfWeights, char[] characterSet){
@@ -63,7 +66,9 @@ public class BWT {
         }
         return sums;
     }
-    //6) ================================= loop through all vectors and apply sum =====================================
+    //
+    //============================ start right to left calculations ================================================
+    //6) =========================== loop through all vectors and apply pre calculated sums ========================
     public  static int[] getNonZeroVectorsSum(char[] arr, int stringLength, int[] uniqueVectorsSum){
         int[] nonZeroSums = new  int[stringLength];
         for(int i = 0; i < stringLength; i++){
@@ -71,31 +76,31 @@ public class BWT {
         }
         return nonZeroSums;
     }
-    //7) ========================================== calculate the sums of zero cells ==================================
+    //7) ================= calculate the sums of zero cells for every column ==================================
     public static int[] addZeroSums(char[] arr,  int[][] vectorsZeroPositions, int[] NonZeroVectorsSum, int[][]
             uniqueVectorsWeights, int stringLength){
-        int[] reference  = uniqueVectorsWeights[arr[stringLength-1]];
-        int[] target;
-        int sum;
-        char col;
-        int ref;
+        int[] referenceColumn  = uniqueVectorsWeights[arr[stringLength-1]];
+        int[] targetColumn;
+        int columnSum;
+        char columnName;
+        int referenceCell;
         for(int i = stringLength-2;i >=0;i--){
-            col = arr[i];
-            target = uniqueVectorsWeights[col];
-            sum = 0;
-            for(int j:vectorsZeroPositions[col]){
-                ref = reference[j + 1];
-                sum +=  ref;
-                target[j] = ref;
+            columnName = arr[i];
+            targetColumn = uniqueVectorsWeights[columnName];
+            columnSum = 0;
+            for(int j:vectorsZeroPositions[columnName]){
+                referenceCell = referenceColumn[j+1];
+                columnSum +=  referenceCell;
+                targetColumn[j] = referenceCell;
             }
-            NonZeroVectorsSum[i] += sum;
-            reference = target;
+            NonZeroVectorsSum[i] += columnSum;
+            referenceColumn = targetColumn;
         }
         return NonZeroVectorsSum;
     }
-    //-----------------------------------------------------------------
+    //---------------------------------------------------------------------------------------------------
     //8) =============================== generate random string =========================================
-    private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZqwertyuiopasdfghjklzxcvbnm[]#',<>?~@|.;:()0123456789";
     public static String randomAlphaNumeric(int count) {
         StringBuilder builder = new StringBuilder();
         int character;
@@ -105,20 +110,20 @@ public class BWT {
         }
         return builder.toString();
     }
-    //9) =============================== generate random string flag = 0 random else rep ===============
+    //9) ========== generate random string flag = 0 random else repeate the given pattern ===============
     public static String generateStringSample(int flag, int rep, int count) {
         String string;
         if(flag == 1){
             string = randomAlphaNumeric(count);
         }else{
             string = "SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES";
-            //string = "sssssssssssssussssssssssspssssssssssssssssss";
             for (int i = 0; i < rep; i++) { string = string + string; }
         }
-        string = string + '$';
+        string = string + '!';
         return string;
     }
-    //10) ============================= generate BWT weights ==================================
+    //=========================================================================================
+    //10) ========== final function to generate BWT weights ===================================
     public  static  int[] generateBwtWeights(String string){
         int stringLength = string.length();
         //------------------------------------------------
@@ -131,10 +136,10 @@ public class BWT {
         int[] finalRes = addZeroSums(arr,  vectorsZeroPositions, NonZeroVectorsSum, uniqueVectorsWeights, stringLength);
         return finalRes;
     }
-    //================================ maim method ========================================
+    //    ================================ maim method ========================================
     //11) ================================ maim method ========================================
     public static void main(String[] args) {
-        //string "SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES";
+        // set flag to 0 to get a string composed or 'rep', set flag to 1 to generate number of random 'count'
         String string = generateStringSample(0, 10, 90000);
         //------------------------------------------------
         long startTime = System.nanoTime();
